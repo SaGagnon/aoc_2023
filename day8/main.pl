4d4d70ecf6dbd84775772a8856226d7da4f14096
Samuel Gagnon <samuel@gagn.ca>
2023-12-09 14:42:57 -0500

    Solution avec LCM.

--- day8/main.pl
+++ day8/main.pl
@@ -1,5 +1,5 @@
 :- use_module(library(rbtrees)).
-
+:- use_module(library(thread)).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Parsing
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -41,9 +41,12 @@ read_instruction(G, r, N, R) :- graph_node_right(G, N, R).
 find_zzz(G, Is, NbSteps) :-
     find_zzz_(G, Is, Is, aaa, 0, NbSteps).
 
-find_zzz_(_, _, _, zzz, NbSteps, NbSteps).
+find_zzz_(_, _, _, Node, NbSteps, NbSteps) :-
+    (   part(2)
+    ->  end_with(z, Node)
+    ;   Node=zzz
+    ).
 find_zzz_(G, Is, [NextI|NextIs], CurNode, NbSteps0, NbSteps) :-
-    dif(CurNode, zzz),
     read_instruction(G, NextI, CurNode, NextNode),
     NbSteps1 #= NbSteps0 + 1,
     find_zzz_(G, Is, NextIs, NextNode, NbSteps1, NbSteps).
@@ -53,6 +56,8 @@ find_zzz_(G, Is, [], CurNode, NbSteps0, NbSteps) :-
 %% solve
 
 solve1(NbSteps) :-
+    abolish(part/1),
+    assertz(part(1)),
     phrase_from_file(map(Is, Neis), 'input.txt'),
     graph_init(Neis, G),
     find_zzz(G, Is, NbSteps).
@@ -61,45 +66,21 @@ solve1(NbSteps) :-
 % Part 2
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Ne fonctionne pas. Il faut utiliser une autre méthode qui n'énumère
-% pas explicitement.
-
-%% predicates for several nodes
-
 end_with(C, N) :- atom_codes(N, [_,_,X]), char_code(C, X).
 
 starting_nodes(G, Ns) :-
     findall(N, (graph_node(G, N), end_with(a, N)), Ns).
 
-end_state(Ns) :-
-    maplist(end_with(z), Ns).
-
-read_instruction_allnodes(G, l, Ns, Ls) :- !, maplist(graph_node_left(G), Ns, Ls).
-read_instruction_allnodes(G, r, Ns, Rs) :- !, maplist(graph_node_right(G), Ns, Rs).
-
-%% find_zzzs
-
 find_zzzs(G, Is, NbSteps) :-
     starting_nodes(G, Ns),
-    find_zzzs_(G, Is, Is, Ns, 0, NbSteps).
-
-find_zzzs_(_, _, _, Nodes, NbSteps, NbSteps) :-
-    end_state(Nodes).
-find_zzzs_(G, Is, [NextI|NextIs], CurNodes, NbSteps0, NbSteps) :- !,
-    \+ end_state(CurNodes),
-    $(read_instruction_allnodes(G, NextI, CurNodes, NextNodes)),
-    NbSteps1 #= NbSteps0 + 1,
-    (   NbSteps1 mod 10000 #= 0
-    ->  print(NbSteps1), nl
-    ;   true
-    ),
-    find_zzzs_(G, Is, NextIs, NextNodes, NbSteps1, NbSteps).
-find_zzzs_(G, Is, [], CurNodes, NbSteps0, NbSteps) :-
-    find_zzzs_(G, Is, Is, CurNodes, NbSteps0, NbSteps).
+    maplist({G,Is}/[N, NbStep]>>find_zzz_(G, Is, Is, N, 0, NbStep), Ns, NbSteps).
 
 %% solve
-
 solve2(NbSteps) :-
+    abolish(part/1),
+    assertz(part(2)),
     phrase_from_file(map(Is, Neis), 'input.txt'),
     graph_init(Neis, G),
     find_zzzs(G, Is, NbSteps).
+
+% Il faut trouver le LCM du résultat
